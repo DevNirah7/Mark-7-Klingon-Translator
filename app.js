a649fd85795cb1049fbe5574a51b9bd5ff457ef9
devnirah <89081343+devnirah@users.noreply.github.com>
2021-09-17 20:53:28 +0530

    done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var inputText = document.querySelector("#txt-input");
 var translateButton = document.querySelector("#btn-translate");
 var outputtext = document.querySelector("#output-div");
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var serverURL = "https://api.funtranslations.com/translate/klingon.json";
 
 function getTranslationURL(text) {
     return serverURL + "?" + "text=" + text
